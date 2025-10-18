3edf516d1aced41c252c1834e47a774ff6787933
devPavanKalyan <mr.kingkalyan@gmail.com>
2025-10-18 15:11:37 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/shopverse.in",
   server: {
     port: 5100
   }
